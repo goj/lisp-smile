1b6913f67ecb22b9a185fb90813adb59948a7bae
Krzysztof.Goj <Krzysztof.Goj@8f4ea72e-d637-0410-b982-73462ad2331f>
2007-08-25 17:27:17 +0000

    shell catches errors

--- make-translator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-OUTPUT=out.scm
-TRANSLATOR='guile translator.scm'
-FILES=(smile.smile stdlib.smile lexer.smile parser.smile translator.smile)
-
-rm $OUTPUT
-for i in ${FILES[@]}; do
-    cat $i  | grep -v '^load' | $TRANSLATOR >> $OUTPUT
-done
